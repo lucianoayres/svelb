c4402aba1fd9b150a09e5c7521d69f1e41d05cc0
Luciano Ayres <lucianofarias@gmail.com>
2022-03-16 10:54:49 -0300

    chore: first commit

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { onMount } from 'svelte'
+  import List from './List.svelte'
+  import Item from './Item.svelte'
+
+  let item
+  let page
+
+  async function hashchange() {
+    const path = window.location.hash.slice(1)
+
+    if (path.startsWith('/item')) {
+      const id = path.slice(6)
+
+      let selectedItem = await fetch('./data/items.json').then((allItemsData) =>
+        allItemsData.json()
+      )
+
+      item = selectedItem.find((tItem) => tItem.id === +id)
+
+      item ? window.scrollTo(0, 0) : alert('article not found')
+    } else if (path.startsWith('/page')) {
+      page = +path.slice(6)
+      item = null
+    } else {
+      window.location.hash = '/page/1'
+    }
+  }
+
+  onMount(hashchange)
+</script>
+
+<svelte:window on:hashchange={hashchange} />
+
+<main>
+  {#if item}
+    <Item {item} returnTo="#/top/{page}" />
+  {:else if page}
+    <List {page} />
+  {/if}
+</main>
+
+<style>
+  main {
+    position: relative;
+    max-width: 800px;
+    margin: 0 auto;
+    min-height: 101vh;
+    padding: 1em;
+  }
+
+  main :global(.meta) {
+    color: #999;
+    font-size: 12px;
+    margin: 0 0 1em 0;
+  }
+
+  main :global(a) {
+    color: rgb(0, 0, 150);
+  }
+</style>
---
+++ src/List.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { beforeUpdate } from 'svelte'
+  import Summary from './Summary.svelte'
+
+  const PAGE_SIZE = 20
+
+  export let page
+
+  let items
+  let offset
+
+  let lastPage
+
+  $: fetch('./data/posts.json')
+    .then((allPosts) => allPosts.json())
+    .then((postsData) => {
+      lastPage = Math.ceil(postsData.length / PAGE_SIZE)
+
+      const startIndex = (page - 1) * PAGE_SIZE
+
+      let pLimit = 0
+      PAGE_SIZE <= postsData.length
+        ? (pLimit = PAGE_SIZE * page)
+        : (pLimit = postsData.length)
+
+      items = postsData.slice(startIndex, pLimit)
+      offset = PAGE_SIZE * (page - 1)
+      window.scrollTo(0, 0)
+    })
+</script>
+
+{#if items}
+  {#each items as item, i}
+    <Summary {item} {i} {offset} />
+  {/each}
+
+  {#if page > 1}
+    <a href="#/page/{page - 1}">previous page</a>
+  {/if}
+
+  {#if page != lastPage}
+    <a href="#/page/{page + 1}">next page</a>
+  {/if}
+{:else}
+  <p class="loading">loading...</p>
+{/if}
+
+{#if page > lastPage}
+  {(window.location.href = '/')}
+{/if}
+
+<style>
+  a {
+    padding: 2em;
+    display: block;
+  }
+
+  .loading {
+    opacity: 0;
+    animation: 0.4s 0.8s forwards fade-in;
+  }
+
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
