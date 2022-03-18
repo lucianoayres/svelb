ee721b4fdb64eb0fa3830f35e534e2689c3daea6
Luciano Ayres <lucianofarias@gmail.com>
2022-03-18 11:10:49 -0300

    feat: add bio

---
+++ src/Bio.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div class="bio">
+  <img
+    class="bio-avatar"
+    src="./images/profile-pic.png"
+    heigth={50}
+    width={50}
+    alt="Luciano Ayres"
+  />
+  <p>Luciano Ayres is ipsum lorem et al lorem ipsum lorem et al ipsum lorem.</p>
+
+  <a href="https://linkedin.com/in/lucianoayres">Follow me on LinkedIn</a>
+</div>
+
+<style>
+</style>
