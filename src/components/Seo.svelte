9c01bb826c7a5b0edb7cd06d43b3b7155085cfc5
Luciano Ayres <lucianofarias@gmail.com>
2022-03-23 16:54:42 -0300

    feat: add SEO component, but still unfinished

---
+++ src/components/Seo.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let title
+  // document.title = title
+
+  // document
+  //   .querySelector('meta[name="description"]')
+  //   .setAttribute('content', 'Hello World Description')
+
+  // console.log(document.querySelector('meta[name="description"]'))
+
+  function test() {
+    console.log('Hello Test')
+  }
+</script>
+
+<svelte:window on:beforeunload={test} />
+
+<!-- <svelte:head>
+  <title>{title}</title>
+  <meta name="description" content="Updated Description" />
+</svelte:head> -->
