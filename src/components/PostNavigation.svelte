69552ff15e423f9b2d99810442f2b6b764a8718c
Luciano Ayres <lucianofarias@gmail.com>
2022-03-31 16:42:51 -0300

    feat: add post navigation component

---
+++ src/components/PostNavigation.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let previousHref
+  export let nextHref
+
+  export let previousLabel = '← Prev Post'
+  export let nextLabel = 'Next Post →'
+</script>
+
+<div class="post-list-navigation">
+  {#if previousHref}
+    <div>
+      <a href={previousHref}>{previousLabel}</a>
+    </div>
+  {/if}
+
+  {#if nextHref}
+    <div>
+      <a href={nextHref}>{nextLabel}</a>
+    </div>
+  {/if}
+</div>
