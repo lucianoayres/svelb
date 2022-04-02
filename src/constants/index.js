3a92d9561f5924ee13547c6c59f2da092cbaf66c
Luciano Ayres <lucianofarias@gmail.com>
2022-04-02 11:26:48 -0300

    chore: move images folder

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
 export const AUTHOR_NAME = 'Luciano Ayres'
 export const AUTHOR_BIO_DESCRIPTION =
   'I write about coding and cloud technology.'
-export const AUTHOR_PROFILE_IMAGE_URL = './images/luciano-ayres-profile-pic.png'
+export const AUTHOR_PROFILE_IMAGE_URL =
+  './images/profile/luciano-ayres-profile-pic.png'
 
 export const AUTHOR_LINKEDIN_URL = 'https://linkedin.com/in/lucianoayres'
 export const AUTHOR_GITHUB_URL = 'https://github.com/lucianoayres'
