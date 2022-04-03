0d88038c84b079fbefeb2e9c394f43bd6722a009
Luciano Ayres <lucianofarias@gmail.com>
2022-04-03 19:18:57 -0300

    chore: modify rss file path

--- src/helpers/generateRss.js
+++ src/helpers/generateRss.js
@@ -21,8 +21,8 @@ const rssFeed = `<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modul
     </item>
   </channel>
 </rss>`
-process.chdir('./')
-fs.writeFile('../public/rss.xml', rssFeed, function (err) {
+process.chdir('./src')
+fs.writeFile('./public/rss.xml', rssFeed, function (err) {
   if (err) {
     return console.log(err)
   }
